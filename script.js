d5474c5933a70c460bc874d594695f2c5c0773d9
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-21 14:27:02 -0700

    Add: Arithmetic functions as first step

---
+++ script.js
@@ -0,0 +1,16 @@
+const add = function(augend, addend) {
+  return augend + addend;
+};
+
+const subtract = function(minuend, subtrahend) {
+  return minuend - subtrahend;
+};
+
+const multiply = function(multiplicand, multiplier) {
+	return multiplicand * multiplier;
+};
+
+const divide = function(dividend, divisor) {
+  return dividend / divisor;
+};
+
