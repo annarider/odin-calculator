d806d4ddea7cddf0cd32a816c8a2cc740c977f4c
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-22 12:39:08 -0700

    Refactor: Use a constructor to store calc state
    I struggled to keep track of all the variables
    and functions I was writing.
    
    Claude told me it's not great to have a lot
    of global variables. It suggested using a
    CalculatorState function which gave me the idea
    to use a constructor.
    
    This refactoring should make it easier to track
    calculator properties and helps me break up the
    event handler into smaller scoped functions while
    passing in the calculator object to track the
    property values.

--- script.js
+++ script.js
@@ -18,25 +18,43 @@ const allClear = function() {
 return display = 0;
 };
 
+function truncateDisplay(calculator) {
+  calculator.displayText = calculator.displayText.slice(0, 11);
+}
 
-const buttons = document.querySelectorAll("button");
-let firstNumber = 0;
-let secondNumber = 0; 
-let operator = '';
+function concatenateDisplay(char, calculator) {
+  if (char.match(/[0-9|.]/)) {
+    calculator.displayText += char;
+    calculator.display.textContent = calculator.displayText; 
+  }
+}
+  
+function Calculator() {
+  this.display = document.querySelector("#display")
+  this.displayText = '0';
+  this.buttons = document.querySelectorAll("button");
+  this.firstNumber = null;
+  this.secondNumber = null;
+  this.operator = null;
+}
+  
+const calculator = new Calculator();
 
-const display = document.querySelector("#display");
-let displayText = '0';
-
-buttons.forEach((button) => {
+calculator.buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     // clear display on first button press
-    if (displayText.length === 1 && displayText === '0') displayText = '';
-    if (displayText.length <= 10) {
+    if (calculator.displayText.length === 1 && calculator.displayText === '0') calculator.displayText = '';
+    // check if overflowing display
+    if (calculator.displayText.length <= 10) {
+      // build display
       const char = e.target.textContent;
-      displayText += char;
-      display.textContent = displayText; 
+      if (char === '.') button.disabled = true;
+      // only show numbers and decimal point in display
+      concatenateDisplay(char, calculator);
+    } else {
+      truncateDisplay(calculator); 
+      ;
     }
-  // const clearButton = document.createElement("clear");
   
   });
 });
@@ -47,3 +65,4 @@ function operate(firstNumber, secondNumber, operator) {
   if (operator === '*') return multiply(firstNumber, secondNumber);
   if (operator === '/') return divide(firstNumber, secondNumber);
 }
+
