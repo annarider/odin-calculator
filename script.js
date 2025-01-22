d8a3ec877bc2fa8637cf36e49509fbb3ed918c0a
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-22 10:25:45 -0700

    Add: Logic for updating display when click buttons
    Updating the display with different logic to
    handle clicking on buttons. Handling the different
    buttons to show on the display the numbers clicked.
    
    The display allows errors, e.g. displaying C and
    multiple decimal points that needs handling.

--- script.js
+++ script.js
@@ -14,9 +14,32 @@ const divide = function(dividend, divisor) {
   return dividend / divisor;
 };
 
+const allClear = function() {
+return display = 0;
+};
+
+
+const buttons = document.querySelectorAll("button");
 let firstNumber = 0;
 let secondNumber = 0; 
 let operator = '';
+
+const display = document.querySelector("#display");
+let displayText = '0';
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    // clear display on first button press
+    if (displayText.length === 1 && displayText === '0') displayText = '';
+    if (displayText.length <= 10) {
+      const char = e.target.textContent;
+      displayText += char;
+      display.textContent = displayText; 
+    }
+  // const clearButton = document.createElement("clear");
+  
+  });
+});
 
 function operate(firstNumber, secondNumber, operator) {
   if (operator === '+') return add(firstNumber, secondNumber);
