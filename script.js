ed8e6ff2c2d7bf1862c1a5ede8954e6a5a9d5f66
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-23 08:48:22 -0700

    Refactor: Split out event listener functions
    It was getting difficult to track the features
    in the event listener. I pulled out distinct
    features and split them into helper functions.
    
    This also required updating existing functions
    so they could call the correct helper function.

--- script.js
+++ script.js
@@ -18,17 +18,46 @@ const allClear = function() {
 return display = 0;
 };
 
-function truncateDisplay(calculator) {
+function truncateDisplay() {
   calculator.displayText = calculator.displayText.slice(0, 11);
 }
 
-function concatenateDisplay(char, calculator) {
-  if (char.match(/[0-9|.]/)) {
-    calculator.displayText += char;
-    calculator.display.textContent = calculator.displayText; 
-  }
+function clearDisplay() {
+  calculator.displayText = '';
 }
-  
+
+function concatenateDisplay(char) {
+  // check if overflowing display
+  if (calculator.displayText.length <= 10) {    
+    calculator.displayText += char;
+  } else {
+    truncateDisplay(calculator)
+  }
+  calculator.display.textContent = calculator.displayText; 
+}
+
+function updateState(char) {
+  if (char.match(/[0-9|.]/)) concatenateDisplay(char);
+  if (char === '=') operate();
+  if (char.match(/[+|-|×|÷]/) && calculator.firstNumber) calculator.operator = char;
+  if (calculator.firstNumber && calculator.operator) {
+    clearDisplay();
+    calculator.secondNumber = Number(calculator.displayText);
+  }
+  calculator.firstNumber = Number(calculator.displayText);
+}
+
+
+function buildDisplay(button, event) {
+  // clear display on first button press
+  if (calculator.displayText.length === 1 && calculator.displayText === '0') clearDisplay();
+  // build display
+  const char = event.target.textContent;
+  if (char === '.') button.disabled = true;
+  // only show numbers and decimal point in display
+  updateState(char);
+  }
+
 function Calculator() {
   this.display = document.querySelector("#display")
   this.displayText = '0';
@@ -37,32 +66,19 @@ function Calculator() {
   this.secondNumber = null;
   this.operator = null;
 }
-  
+
 const calculator = new Calculator();
 
 calculator.buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    // clear display on first button press
-    if (calculator.displayText.length === 1 && calculator.displayText === '0') calculator.displayText = '';
-    // check if overflowing display
-    if (calculator.displayText.length <= 10) {
-      // build display
-      const char = e.target.textContent;
-      if (char === '.') button.disabled = true;
-      // only show numbers and decimal point in display
-      concatenateDisplay(char, calculator);
-    } else {
-      truncateDisplay(calculator); 
-      ;
-    }
-  
+    buildDisplay(button, e);
   });
 });
 
-function operate(firstNumber, secondNumber, operator) {
-  if (operator === '+') return add(firstNumber, secondNumber);
-  if (operator === '-') return subtract(firstNumber, secondNumber);
-  if (operator === '*') return multiply(firstNumber, secondNumber);
-  if (operator === '/') return divide(firstNumber, secondNumber);
+function operate() {
+  if (calculator.operator === '+') add(calculator.firstNumber, calculator.secondNumber);
+  if (calculator.operator === '-') subtract(calculator.firstNumber, calculator.secondNumber);
+  if (calculator.operator === '*') multiply(calculator.firstNumber, calculator.secondNumber);
+  if (calculator.operator === '/') divide(calculator.firstNumber, calculator.secondNumber);
 }
 
